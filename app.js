5396e65928272e115beff43781e2c9cfae57a245
Graciela Zaera <graciela.zaera@unify.com>
2015-06-06 17:51:18 +0200

    cleaning up
    Clean up components folder

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'client')));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 /*
 app.get('/', function(req, res){
